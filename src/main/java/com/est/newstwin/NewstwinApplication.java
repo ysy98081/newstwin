78adeb8c8d4fd2488b6d8468c6eb8cc89902cd68
voco <vocochoe@gmail.com>
2025-10-30 10:51:33 +0900

    Resolve: .gitignore merge conflict during rebase
    - 충돌 해결

---
+++ src/main/java/com/est/newstwin/NewstwinApplication.java
@@ -0,0 +1,13 @@
+package com.est.newstwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstwinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewstwinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/est/newstwin/NewstwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.newstwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstwinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
